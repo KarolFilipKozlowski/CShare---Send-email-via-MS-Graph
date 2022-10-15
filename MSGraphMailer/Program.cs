3c6e8c181b399335bddc0c65ad73869b1e283847
Karol Kozłowski <ravenweb@gmail.com>
2022-10-15 19:23:15 +0200

    Update Program.cs

--- MSGraphMailer/Program.cs
+++ MSGraphMailer/Program.cs
@@ -9,8 +9,8 @@ namespace MSGraphMailer
         {
             var graphClient = new GetGraphServiceClient(AuthorizedBy.byClientSecret);
             UserSendMail.SendMail(graphClient.GraphClient, 
-                sernder: "alexw@4ql5ky.contoso.com", 
-                toRecipients: new string[] { "lidiah@4ql5ky.contoso.com", "henriettam@4ql5ky.contoso.com" },
+                sernder: "alexw@contoso.com", 
+                toRecipients: new string[] { "lidiah@contoso.com", "henriettam@contoso.com" },
                 subject: "Send mail",
                 boodyContent: "Send the <b>message</b> specified in the request body using either JSON or MIME format.", bodyContentType: BodyType.Html,
                 attachments: new string[] { @"C:\temp\user_sendMail.pdf" }
